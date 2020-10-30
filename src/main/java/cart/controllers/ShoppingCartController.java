886808be8e59581050873f081ba7347a271f2997
MenilikBelay <menilikbelay@gmail.com>
2020-10-30 10:33:02 -0500

    add CORS policy for ShoppingCartController

--- src/main/java/cart/controllers/ShoppingCartController.java
+++ src/main/java/cart/controllers/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package cart.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import cart.models.Product;
 import cart.services.ShoppingCartService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/cart/{userId}")
 public class ShoppingCartController {
 
